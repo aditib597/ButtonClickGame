63a6d990f9f90a5cba01146ae724a81b8db24ab3
Aditi B <aditib575@gmail.com>
2020-07-23 12:29:26 -0700

    created user interface and added functionality

--- ButtonClick/ContentView.swift
+++ ButtonClick/ContentView.swift
@@ -9,6 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var score = 0
+    @State var timeLeft = 20
+    @State var buttonDisabled = false
+    @State var text = "Start"
     var body: some View {
         VStack {
             Spacer()
@@ -16,8 +20,16 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundColor(Color.black)
+            Spacer()
+            Text("Score: \(score)").font(.system(size: 40, weight: .semibold, design: .rounded))
+            Text("Time: \(timeLeft)").font(.system(size: 40, weight: .semibold, design: .rounded))
+            Spacer()
+            Button(action: {self.startGame()}) {
+                Text(text).font(.system(size: 70, weight: .semibold, design: .rounded))
+            }
           Spacer()
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Button(action: {self.buttonPress()
+            }) {
             Text("Click Me")
                 .font(.system(size: 50, weight: .heavy, design: .rounded))
                 .foregroundColor(Color.white)
@@ -25,10 +37,50 @@ struct ContentView: View {
         .background(Color.pink)
         .clipShape(Circle())
             .padding(.horizontal, 50.0)
+        .disabled(buttonDisabled)
             Spacer()
+        }//vstack
+    }//var body
+    func startGame() {
+                 if timeLeft == 20 {
+                    countDown()
+                 }
+           if timeLeft == 0 {
+               timeLeft = 20
+               score = 0
+               text = "Start"
+               buttonDisabled = false
+           }
+             }
+    //func startgame
+    func countDown() {
+        
+        if timeLeft > 0 {
+        let seconds = 1.0
+            DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            self.timeLeft = self.timeLeft - 1
+            self.countDown()
+                  }
+              } else {
+                text = "Start Over"
+                buttonDisabled = true
+              }
+        }//func countdown
+    func buttonPress() {
+            print("upping score")
+            score = score + 1
+    }//func buttonpress
+    
+    func disable() {
+        if timeLeft == 0 {
+            buttonDisabled = true
+        } else {
+            buttonDisabled = false
         }
-    }
-}
+    }//func disable
+    
+}//content view
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
