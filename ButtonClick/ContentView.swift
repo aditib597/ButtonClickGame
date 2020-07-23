f01c7557ed0cf8992dca3dc87d6047f51047781b
Aditi B <aditib575@gmail.com>
2020-07-23 11:40:57 -0700

    Created user interface

--- ButtonClick/ContentView.swift
+++ ButtonClick/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  ButtonClick
 //
-//  Created by P Kris on 7/23/20.
+//  Created by Aditi on 7/23/20.
 //  Copyright © 2020 Aditi. All rights reserved.
 //
 
@@ -10,7 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            Text("Button Game")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.black)
+          Spacer()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Click Me")
+                .font(.system(size: 50, weight: .heavy, design: .rounded))
+                .foregroundColor(Color.white)
+        }.frame(width: 300.0, height: 300.0)
+        .background(Color.pink)
+        .clipShape(Circle())
+            .padding(.horizontal, 50.0)
+            Spacer()
+        }
     }
 }
 
